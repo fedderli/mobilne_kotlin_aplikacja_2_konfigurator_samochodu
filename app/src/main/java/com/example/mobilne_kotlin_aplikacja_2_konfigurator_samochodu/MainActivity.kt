f4de0b2d5a7e00488f191a78a0d223c5ced2b2d1
moje <twojemail@example.com>
2025-03-23 16:12:53 +0100

    Add RadioButton, CheckBox functionality in MainActivity

--- app/src/main/java/com/example/mobilne_kotlin_aplikacja_2_konfigurator_samochodu/MainActivity.kt
+++ app/src/main/java/com/example/mobilne_kotlin_aplikacja_2_konfigurator_samochodu/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.example.mobilne_kotlin_aplikacja_2_konfigurator_samochodu
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.ImageView
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +22,60 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val radioGroup = findViewById<RadioGroup>(R.id.carRadioGroup)
+        val leatherSeat = findViewById<CheckBox>(R.id.CheckBoxLeatherSeat)
+        val airCondition = findViewById<CheckBox>(R.id.CheckBoxAirConditioning)
+        val orderButton = findViewById<Button>(R.id.orderButton)
+        val carImageView = findViewById<ImageView>(R.id.coffyImigeView)
+        val carOrder = findViewById<TextView>(R.id.carOrder)
+
+
+        var whichCar = ""
+
+
+
+        radioGroup.setOnCheckedChangeListener { _, checkedId ->
+            val radioButton = findViewById<RadioButton>(checkedId)
+            whichCar = "${radioButton.text}"
+            when (checkedId) {
+                R.id.suv_radiobutton -> carImageView.setImageResource(R.drawable.suv)
+                R.id.sedan_radiobutton -> carImageView.setImageResource(R.drawable.sedan)
+                R.id.hatchback_radiobutton -> carImageView.setImageResource(R.drawable.hatchback)
+            }
+        }
+
+
+
+
+        var airConditionChecked = ""
+        var leatherSeatCheked = ""
+
+        airCondition.setOnCheckedChangeListener{_ , isChecked ->
+            if (isChecked){
+                airConditionChecked = "Tak"
+            }
+            else {
+                airConditionChecked = "Nie"
+            }
+        }
+
+        leatherSeat.setOnCheckedChangeListener{_ , isChecked ->
+            if (isChecked){
+                leatherSeatCheked = "Tak"
+            }
+            else {
+                leatherSeatCheked = "Nie"
+            }
+        }
+
+        orderButton.setOnClickListener{
+            carOrder.text = "wybrałes : $whichCar \n" +
+                    "wentylacja: $airConditionChecked \n" +
+                    "skorzane siedzenia $leatherSeatCheked"
+        }
+
+
     }
+
 }
